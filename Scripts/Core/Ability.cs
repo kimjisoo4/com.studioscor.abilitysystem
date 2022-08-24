928d95533a9cbdd3b2a14ab2213db3054f65f60a
kimjisoo4 <58765606+kimjisoo4@users.noreply.github.com>
2022-08-25 07:25:36 +0900

    보기 좋은 글이 쓰기도 편하다
     [ Added ]
     Odin
     - 오딘 인스펙터를 사용할 때, 인스펙터의 표기 방법을 변경함.

--- Scripts/Core/Ability.cs
+++ Scripts/Core/Ability.cs
@@ -2,26 +2,63 @@
 using System.Collections;
 using System.Collections.Generic;
 
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
 namespace KimScor.GameplayTagSystem.Ability
 {
     public abstract class Ability : ScriptableObject
     {
+#if ODIN_INSPECTOR
+        [VerticalGroup("Ability/Properties")]
+        [PropertyOrder(1)]
+#else
         [Header(" 어빌리티의 이름 ")]
+#endif
         [SerializeField] protected string _AbilityName;
 
+#if ODIN_INSPECTOR
+        [PropertyOrder(0)]
+        [HorizontalGroup("Ability", Width = 50), HideLabel, PreviewField(50)]
+#else
         [Header(" 어빌리티의 아이콘")]
+#endif
         [SerializeField] protected Sprite _AbilityIcon;
 
+#if ODIN_INSPECTOR
+        [VerticalGroup("Ability/Properties")]
+        [PropertyOrder(3)]
+        [EnumToggleButtons]
+#else
         [Header(" 어빌리티의 종류")]
+#endif
         [SerializeField] protected EAbilityType _AbilityType;
 
+#if ODIN_INSPECTOR
+        [BoxGroup("Gameplay Tags")]
+        [PropertyOrder(0)]
+#else
         [Header(" 어빌리티의 태그 ")]
+#endif
         [SerializeField] private GameplayTag _AbilityTag;
 
+#if ODIN_INSPECTOR
+        [BoxGroup("Gameplay Tags")]
+        [PropertyOrder(1)]
+        [ListDrawerSettings(Expanded = true)]
+#else
         [Header(" 어빌리티의 속성 ")]
+#endif
         [SerializeField] private GameplayTag[] _AttributeTags;
 
+#if ODIN_INSPECTOR
+        [BoxGroup("Gameplay Tags")]
+        [PropertyOrder(2)]
+        [ListDrawerSettings(Expanded = true)]
+#else
         [Header(" 어빌리티 취소")]
+#endif
         [SerializeField] private GameplayTag[] _CancelTags;
 
         public string AbilityName => _AbilityName;
--- Scripts/Core/FAbilityTags.cs
+++ Scripts/Core/FAbilityTags.cs
@@ -1,38 +1,64 @@
 ﻿using UnityEngine;
 
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
 namespace KimScor.GameplayTagSystem.Ability
 {
     [System.Serializable]
     public struct FAbilityTags
     {
         /// <summary>
+        /// 필요 태그
+        /// </summary>
+#if ODIN_INSPECTOR
+        [BoxGroup("Condition Tags")]
+        [ListDrawerSettings(Expanded = true)]
+#endif
+        [Header("활성화 필수 태그")]
+        public GameplayTag[] RequiredTags;
+        /// <summary>
+        /// 방해 태그
+        /// </summary>
+#if ODIN_INSPECTOR
+        [BoxGroup("Condition Tags")]
+        [ListDrawerSettings(Expanded = true)]
+#endif
+        [Header("활성화 차단 태그")]
+        public GameplayTag[] ObstacledTags;
+
+        /// <summary>
         /// 부여 소유 태그
         /// </summary>
+#if ODIN_INSPECTOR
+        [BoxGroup("Added Tags")]
+        [ListDrawerSettings(Expanded = true)]        
+#endif
         [Header(" 활성화 부여 태그 ")]
         public GameplayTag[] AddOwnedTags;
 
         /// <summary>
         /// 부여 차단 태그
         /// </summary>
+#if ODIN_INSPECTOR
+        [BoxGroup("Added Tags")]
+        [ListDrawerSettings(Expanded = true)]
+#endif
         [Header(" 어빌리티 차단 태그 ")]
         public GameplayTag[] AddBlockTags;
 
         /// <summary>
         /// 캔슬 태그
         /// </summary>
+#if ODIN_INSPECTOR
+        [BoxGroup("Cancel Tags")]
+        [ListDrawerSettings(Expanded = true)]
+#endif
         [Header(" 어빌리티 취소 태그 ")]
         public GameplayTag[] CancelAbilityTags;
 
-        /// <summary>
-        /// 필요 태그
-        /// </summary>
-        [Header("활성화 필수 태그")]
-        public GameplayTag[] RequiredTags;
-        /// <summary>
-        /// 방해 태그
-        /// </summary>
-        [Header("활성화 차단 태그")]
-        public GameplayTag[] ObstacledTags;
+
 
         public FAbilityTags(GameplayTag abilityTag, GameplayTag[] attributeTags, GameplayTag[] activationOwnedTags, GameplayTag[] blockAbilitiesWithTag, GameplayTag[] cancelAbilitiesWithTag, GameplayTag[] activationRequiredTags, GameplayTag[] activationBlockedTags)
         {
